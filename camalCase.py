23251e786485e6859ec682602bdd2a0cf50cd2ca
xm6264jz <ahmed.abdinoor3@gmail.com>
2020-09-07 11:42:55 -0500

    sentence Camel case

---
+++ camalCase.py
@@ -0,0 +1,15 @@
+def camelcase(sentence):
+	""" Convert sentence to camelCase, for example, "Display all books" 
+	is converted to "displayAllBooks" """
+	title_case = sentence.title() # Uppercase first letter of each word
+	upper_camel_cased = title_case.replace(' ', '') # remove spaces 
+	# Lowercase first letter, join with rest of string 
+	# Slices don't produce index out of bounds errors. 
+	# So this still works on empty strings, strings of length 1
+	return upper_camel_cased[0:1].lower() + upper_camel_cased[1:] 
+def main():
+	sentence = input('Enter your sentence: ')
+	output = camelcase(sentence)
+	print(output)
+if __name__ == '__main__':
+	main()
