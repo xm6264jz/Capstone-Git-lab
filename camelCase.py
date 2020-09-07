8d9eaf0b18c78bc416169190c666fd6f2ec03362
xm6264jz <ahmed.abdinoor3@gmail.com>
2020-09-07 11:48:34 -0500

    add program name banner

--- camelCase.py
+++ camelCase.py
@@ -6,8 +6,16 @@ def camelcase(sentence):
 	# Lowercase first letter, join with rest of string 
 	# Slices don't produce index out of bounds errors. 
 	# So this still works on empty strings, strings of length 1
-	return upper_camel_cased[0:1].lower() + upper_camel_cased[1:] 
+	return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
+
+def display_banner():
+	""" Display program name in banner """
+	msg = 'AWSOME camelCaseGenerator PROGRAM'
+	stars = '*' * len(msg)
+	print(f'\n {stars} \n {msg} \n {stars}\n') 
+
 def main():
+	display_banner()
 	sentence = input('Enter your sentence: ')
 	output = camelcase(sentence)
 	print(output)
